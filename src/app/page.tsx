ba648a674e0093fb2842b22528ea5a008a2fac3e
hqman <hqmank@gmail.com>
2025-02-01 15:44:32 +0800

    Refactor home page layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-800">
-      <main className="flex flex-col gap-8 row-start-2 items-center ">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
         <p className="text-4xl font-bold text-center text-white">This is Next Auth Demo</p>
         <div className="flex  items-center gap-4 pl-10">
           <Link href="/dashboard" className="px-4 py-2 bg-violet-500 text-white rounded-md hover:bg-violet-600 transition-colors duration-200">Dashboard</Link>
